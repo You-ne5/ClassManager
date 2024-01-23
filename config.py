3bd6f448f04c8c072e3aa9cd7bd32219adb20a27
You-ne5 <erenyounes2006@gmail.com>
2024-01-24 00:31:02 +0100

    add embed color constant

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ load_dotenv()
 
 from os import environ as env
 
+#discord
 DISCORD_TOKEN = env["TOKEN"]
 DLG_ID : int = int(env["DLG_ID"])
 ANNOUNCE_CHANNEL_ID : int = int(env["ANNOUNCE_CHANNEL_ID"])
@@ -11,3 +12,6 @@ ADMIN_ROLE_ID : int = int(env["ADMIN_ROLE_ID"])
 HELP_CATEGORY_ID : int = int(env["HELP_CATEGORY_ID"])
 LESSONS_CATEGORY_ID : int = int(env["LESSONS_CATEGORY_ID"])
 HELP_ARCHIVE_CATEGORY_ID : int = int(env["HELP_ARCHIVE_CATEGORY_ID"])
+
+#code
+EMBED_COLOR = 0Xd4af37
