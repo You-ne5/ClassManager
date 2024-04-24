8304f147f11ab9c4d3af91d1fdebf4ceff0681aa
You-ne5 <erenyounes2006@gmail.com>
2024-04-25 00:22:10 +0100

    feat : add db setup to config

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-
+import aiosqlite
 load_dotenv()
 
 from os import environ as env
@@ -11,8 +11,13 @@ ANNOUNCE_CHANNEL_ID : int = int(env["ANNOUNCE_CHANNEL_ID"])
 ADMIN_ROLE_ID : int = int(env["ADMIN_ROLE_ID"])
 HELP_CATEGORY_ID : int = int(env["HELP_CATEGORY_ID"])
 LESSONS_CATEGORY_ID : int = int(env["LESSONS_CATEGORY_ID"])
-HELP_ARCHIVE_CATEGORY_ID : int = int(env["HELP_ARCHIVE_CATEGORY_ID"])
 CALCULATOR_CHANNEL_ID : int = int(env["CALCULATOR_CHANNEL_ID"])
 
 #code
 EMBED_COLOR = 0Xd4af37
+
+async def get_db():
+    db = await aiosqlite.connect("main.db")
+    cursor = await db.cursor()
+
+    return (db, cursor)
