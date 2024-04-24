337ce25aa72b8e8caf309f0ef4589d5e4cfc11dc
You-ne5 <erenyounes2006@gmail.com>
2024-04-25 00:23:47 +0100

    light refactor + handle error

--- utils/views.py
+++ utils/views.py
@@ -1,7 +1,9 @@
+import aiosqlite
 from nextcord import *
 from nextcord.interactions import Interaction
-from config import HELP_CATEGORY_ID, HELP_ARCHIVE_CATEGORY_ID, EMBED_COLOR
+from config import HELP_CATEGORY_ID, EMBED_COLOR
 from nextcord.ext.commands import Bot
+from utils.functions import get_hac_id, create_new_hac
 
 
 class Confirm(ui.View):
@@ -111,7 +113,7 @@ class HelpPanel(ui.View):
     
     @ui.button(label="Close channel", style=ButtonStyle.red, custom_id="close_helpchannel_button")
     async def close_help(self, button: ui.Button, interaction: Interaction):
-
+        
         if interaction.channel.name.startswith(interaction.user.nick.split()[0].lower()) or self.client.is_owner(interaction.user):
 
             confirm_embed = Embed(
@@ -127,6 +129,8 @@ class HelpPanel(ui.View):
             if confirm_view.value:
 
                 perms = {interaction.guild.default_role : PermissionOverwrite(view_channel=True, send_messages=False)}
+                HELP_ARCHIVE_CATEGORY_ID = await get_hac_id(interaction.guild.id, self.client.cursor)
+                print(HELP_ARCHIVE_CATEGORY_ID)
                 help_archive_category : CategoryChannel = interaction.guild.get_channel(HELP_ARCHIVE_CATEGORY_ID)
                 archive_channel_name = f"{interaction.channel.name}-1"
 
@@ -139,10 +143,8 @@ class HelpPanel(ui.View):
                 try:
                     await interaction.channel.edit(name=archive_channel_name, category=help_archive_category, overwrites=perms)
                 except errors.HTTPException:
-                    channels = help_archive_category.channels
-                    await channels[0].delete()
+                    help_archive_category = await create_new_hac(interaction)
                     await interaction.channel.edit(name=archive_channel_name, category=help_archive_category, overwrites=perms)
-
                 await interaction.channel.send(embed=Embed(title="Channel closed", color=EMBED_COLOR))
                 
             else:
