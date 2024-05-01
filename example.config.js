8977bab84f1f372d2dcedc89c1a9bd0eee278a8e
You-ne5 <erenyounes2006@gmail.com>
2024-05-01 16:44:37 +0100

    setup auto deploy

---
+++ example.config.js
@@ -0,0 +1,17 @@
+module.exports= {
+    apps: [
+        {
+            name: "class-manager",
+            script: "client.py",
+            env: {
+                TOKEN: "",
+                DLG_ID: "",
+                ANNOUNCE_CHANNEL_ID: "",
+                ADMIN_ROLE_ID: "",
+                HELP_CATEGORY_ID: "",
+                LESSONS_CATEGORY_ID: "",
+                CALCULATOR_CHANNEL_ID: "",
+            }
+        }
+    ]
+}
